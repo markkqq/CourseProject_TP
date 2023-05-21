8feff3b20188c7307cbe0e3a2b8a777c2169af1a
Марк Дёмин <mdemin22112002@gmail.com>
2023-05-21 16:48:54 +0700

    /

---
+++ CourseProject_TP/CourseProject_TP/RelayCommand.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+using System.Windows.Input;
+using System.Diagnostics;
+namespace CourseProject_TP
+{
+    public class RelayCommand : ICommand
+    {
+        readonly Action<object> _execute;
+        readonly Predicate<object> _canExecute;
+
+        public RelayCommand(Action<object> execute, Predicate<object> canExecute)
+        {
+            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            _canExecute = canExecute;
+        }
+
+        public RelayCommand(Action<object> execute)
+            : this(execute, null)
+        {
+        }
+
+        [DebuggerStepThrough]
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute == null ? true : _canExecute(parameter);
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute(parameter);
+        }
+    }
+}
---
+++ CourseProject_TP/CourseProject_TP/ViewModel/TournamentViewModels/TournamentViewModel.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Collections.ObjectModel;
+using CourseProject_TP.Model;
+namespace CourseProject_TP.ViewModel
+{
+    public class TournamentViewModel : ViewModelBase
+    {
+        Tournament _tournament;
+        private readonly MainWindowViewModel mwvm;
+        private GameSessionShortViewModel selectedGameSession;
+        public TournamentViewModel(Tournament tournament, MainWindowViewModel mainWindowViewModel)
+        {
+            _tournament = tournament;
+            var gameSessionViewModels = from gameSession in tournament.GameSessions select new GameSessionShortViewModel(gameSession);
+            GameSessions = new ObservableCollection<GameSessionShortViewModel>(gameSessionViewModels);
+            mwvm = mainWindowViewModel;
+        }
+        public string Name
+        {
+            get { return _tournament.Name; }
+            set
+            {
+                if (_tournament.Name == value)
+                {
+                    return;
+                }
+                _tournament.Name = value;
+                OnPropertyChanged();
+            }
+        }
+        public ObservableCollection<GameSessionShortViewModel> GameSessions { get; set; }
+
+        public ICommand ShowGameSession
+        {
+            get
+            {
+                return new RelayCommand
+                    (
+                        (_) => ShowGameSessionImplemention(),
+                        (_) => CanShowGameSession()
+                    );
+            }
+        }
+        public GameSessionShortViewModel SelectedGameSession
+        {
+            get => selectedGameSession;
+            set
+            {
+                if (selectedGameSession == value) return;
+                selectedGameSession = value;
+                OnPropertyChanged();
+            }
+        }
+        private bool CanShowGameSession()
+        {
+            return selectedGameSession != null;
+        }
+
+        private void ShowGameSessionImplemention()
+        {
+            GameSession gameSession = _tournament.GameSessions.First(x => $"{x.Date.Year}, {x.Date.Month}" == selectedGameSession.Date);
+            mwvm.Content = new GameSessionViewModel(gameSession,this,mwvm);
+        }
+    }
+}
